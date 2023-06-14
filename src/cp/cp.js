6ac2f586e7b962726cfa96ed38cb367b6ba21736
necket <nik.sherbakov.01@gmail.com>
2023-06-15 01:44:57 +0300

    Child Processes (src/cp)

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from 'child_process';
+
+const scriptPath = './src/cp/files/script.js';
+
+const spawnChildProcess = async (args = []) => {
+    const childProcess = spawn('node', [scriptPath, ...args]);
+
+    process.stdin.pipe(childProcess.stdin);
+    childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['--arg1', 'value1', '--arg2', 'value2']);
