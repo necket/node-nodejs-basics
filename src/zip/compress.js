e8d3f541e8d27578eb017448ed29813a4922bc36
necket <nik.sherbakov.01@gmail.com>
2023-06-14 21:02:14 +0300

    Zlib (src/zip)

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+
 const compress = async () => {
-    // Write your code here 
+    const zip = createGzip();
+    const input = createReadStream('./src/zip/files/fileToCompress.txt');
+    const output = createWriteStream('./src/zip/files/archive.gz');
+
+    pipeline(input, zip, output, (error) => {
+        if (error) throw new Error(error.message);
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+import { pipeline } from 'stream';
+
 const decompress = async () => {
-    // Write your code here 
+    const upzip = createGunzip();
+    const input = createReadStream('./src/zip/files/archive.gz');
+    const output = createWriteStream('./src/zip/files/fileToCompress.txt');
+
+    pipeline(input, upzip, output, (error) => {
+        if (error) throw new Error(error.message);
+    });
 };
 
 await decompress();
