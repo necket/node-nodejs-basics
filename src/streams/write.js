442d14e341b535cac6d0792ca14db49cbb1f7969
necket <nik.sherbakov.01@gmail.com>
2023-06-14 20:14:09 +0300

    Streams (src/streams)

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,8 @@
+import { createWriteStream } from 'fs';
+
 const write = async () => {
-    // Write your code here 
+    const ws = createWriteStream('./src/streams/files/fileToWrite.txt');
+    process.stdin.pipe(ws);
 };
 
 await write();
